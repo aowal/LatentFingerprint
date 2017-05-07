9ae72c04e483ca2efa6cd71c5a5a756e16cdea43
shankar agarwal <sagarwal5@walmartlabs.com>
2017-05-07 18:38:37 +0530

    Adding Files

--- src/similarity/GetMostSimilarMatrix.m
+++ src/similarity/GetMostSimilarMatrix.m
@@ -11,22 +11,39 @@
 
 function [sim,result] = GetMostSimilarMatrix(dictMatrix, latentPatch)
 
-    [N,M] = size(dictMatrix);
-    size(dictMatrix);
-    sim  = 0;
+    [n,m] = size(dictMatrix);
+    currSimScore = 0;
+    sim  = 0
     result = [];
     
-    for idx=1:N
-        currRegionMatrix = dictMatrix(idx); 
-        currRegionMatrix = cell2mat(currRegionMatrix);
-%         currSimScore = sum(sum(pdist2(currRegionMatrix,latentPatch,'jaccard')))
-%         currSimScore = 1.0000 - currSimScore
-        currSimScore = numel(find(currRegionMatrix==latentPatch))/numel(currRegionMatrix);
-        if(sim<currSimScore)
-            sim=currSimScore;
-            result = currRegionMatrix;
+    for idx=1:n
+        currRegionMatrix = dictMatrix{idx};
+        Count1=size(currRegionMatrix,1); Count2=size(latentPatch,1); n=0;
+        T=15;  %Threshold for distance
+        TT=14; %Threshold for theta
+        for i=1:Count1
+            Found=0; j=1;
+            while (Found==0) && (j<=Count2)
+                dx=(currRegionMatrix(i,1)-latentPatch(j,1));
+                dy=(currRegionMatrix(i,2)-latentPatch(j,2));
+                d=sqrt(dx^2+dy^2);    %Euclidean Distance between T1(i) & T2(i)
+                if d<T
+                    DTheta=abs(currRegionMatrix(i,3)-latentPatch(j,3))*180/pi;
+                    DTheta=min(DTheta,360-DTheta);
+                    if DTheta<TT
+                        n=n+1;        %Increase Score
+                        Found=1;
+                    end
+                end
+                j=j+1;
+            end
         end
- 
+    currSimScore=sqrt(n^2/(Count1*Count2));       %Similarity Index
+    
+    if(sim>currSimScore)
+        sim=sim;
+        result = currRegionMatrix
     end
     
+    end
 end
